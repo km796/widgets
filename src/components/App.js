7e624fbf498205045b1f5f9de3ef5d2916d2af8c
km796 <31836577+km796@users.noreply.github.com>
2022-03-24 13:13:18 +0900

    Accordion Component Added and Styled

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,21 @@
 import React from "react"
+import Accordion from "./Accordion"
+
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front ent js framework'
+    }, 
+    {
+        title: 'Why use React?',
+        content: 'Makes front end development easier'
+    }
+]
 
 export default () => {
-    return <h1>Widgets App</h1>
+    return (
+        <div>
+            <Accordion items={items}/>
+        </div>
+    )
 } 
