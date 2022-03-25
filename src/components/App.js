22ab83c2f87e6f320c82cf0c7ba72c662504a176
km796 <31836577+km796@users.noreply.github.com>
2022-03-25 11:33:44 +0900

    Setup for Search component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Accordion from "./Accordion"
+import Search from "./Search"
 
 const items = [
     {
@@ -15,7 +16,7 @@ const items = [
 export default () => {
     return (
         <div>
-            <Accordion items={items}/>
+            <Search />
         </div>
     )
 } 
