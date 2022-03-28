fa634aeb4a182fa7ec4b2b5a848105d377473bae
km796 <31836577+km796@users.noreply.github.com>
2022-03-28 18:11:36 +0900

    Throttling network request

--- src/components/Search.js
+++ src/components/Search.js
@@ -19,10 +19,22 @@ const Search = () => {
 
             setResults(data.query.search)
         }
-        
-        if (term){
+
+        if (term && !results.length) {
             search()
+        } else {
+            const timeoutId = setTimeout(() => {
+                if (term){
+                    search()
+                }
+            }, 1000)
+    
+            return () => {
+                clearTimeout(timeoutId)
+            }
         }
+        
+        
     }, [term])
 
     const renderedResults = results.map((result) => {
