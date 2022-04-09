ae79ec02287cf1771e666faa4f28893f61c9c899
km796 <31836577+km796@users.noreply.github.com>
2022-04-09 16:44:22 +0900

    Google Translate API integration

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
 import Dropdown from './Dropdown'
+import Convert from './Convert'
+
+// AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM
+
 
 const options = [
     {
@@ -36,6 +40,9 @@ const Translate = () => {
             selected={language}
             onSelectedChange={setLanguage}
             options={options}/>
+            <hr />
+            <h3 className='ui header'>Output</h3>
+            <Convert text={text} language={language}/>
         </div>
     )
 }
